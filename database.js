1c3803baddadfa6b68984edfaf7e867a03068d82
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-02-05 20:39:43 +1000

    update

--- database.js
+++ database.js
@@ -10,6 +10,34 @@ app.post("/subscribe", async (req, res) => {
   res.json({ status: "OK" });
 });
 
+app.get("/secret-list-of-emails", async (req, res) => {
+  let data;
+  try {
+    data = await fs.readFile("emails.txt", "utf-8");
+  } catch (e) {
+    res.send(``);
+    res.end();
+    return;
+  }
+
+  let emails = new Set([
+    ...data
+      .split("\n")
+      .map((x) => x.trim())
+      .filter((x) => x.includes("@"))
+  ]);
+  const html = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <body>
+    ${Array.from(emails).map((x) => `<div>${x}</div>`).join("")}
+    </body>
+    </html>
+  `;
+
+  res.send(html);
+});
+
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
 });
